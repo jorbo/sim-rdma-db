16f8690412b38b71f254411e22bbfe5ec5aba2e6
Jordan <jaayvazian@gmail.com>
2026-04-15 14:52:48 -0700

    Remove redundant internal and insert tests from main function for cleaner output

--- krnl/test/main.cpp
+++ krnl/test/main.cpp
@@ -24,40 +24,6 @@ int main() {
 		std::cerr << "\nFailed!\n" << std::endl;
 		failed++;
 	}
-	std::cout << "--- One Internal ---" << std::endl;
-	if (one_internal(&root, hbm, req_buffer, resp_buffer, loop_max, op_max, reset)) {
-		std::cout << "\nPassed!\n" << std::endl;
-		passed++;
-	} else {
-		std::cerr << "\nFailed!\n" << std::endl;
-		failed++;
-	}
-
-	std::cout << "\n\n=== Insert Tests ===" << std::endl;
-	std::cout << "--- Leaf Node ---" << std::endl;
-	if (leaf_node(&root, hbm, req_buffer, resp_buffer, loop_max, op_max, reset)) {
-		std::cout << "\nPassed!\n" << std::endl;
-		passed++;
-	} else {
-		std::cerr << "\nFailed!\n" << std::endl;
-		failed++;
-	}
-	std::cout << "--- Split Root ---" << std::endl;
-	if (split_root(&root, hbm, req_buffer, resp_buffer, loop_max, op_max, reset)) {
-		std::cout << "\nPassed!\n" << std::endl;
-		passed++;
-	} else {
-		std::cerr << "\nFailed!\n" << std::endl;
-		failed++;
-	}
-	std::cout << "--- Insert Until It Breaks ---" << std::endl;
-	if (until_it_breaks(&root, hbm, req_buffer, resp_buffer, loop_max, op_max, reset)) {
-		std::cout << "\nPassed!\n" << std::endl;
-		passed++;
-	} else {
-		std::cerr << "\nFailed!\n" << std::endl;
-		failed++;
-	}
 
 	std::cout << "\n" << passed << " tests passed, "
 		<< failed << " tests failed." << std::endl;
